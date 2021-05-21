9a2e3d41ad115204b7f619d2fc1bbb9039530e2a
NHagar <nicholasrhagar@gmail.com>
2021-05-21 14:14:35 -0500

    file structure stuff

--- newsletter_collect.py
+++ newsletter_collect.py
@@ -17,9 +17,45 @@ DATA_PATH = pathlib.Path("./data/")
 # Collect posts
 
 class Newsletter:
-    def __init__(self):
-        ""
+    def __init__(self, nlobj):
+        self.id = nlobj['id']
+        self.host = nlobj['base_url']
+        self.index_endpoint = f"{self.host}/api/v1/archive"
+        self.post_endpoint = f"{self.host}/api/v1/posts/"
+    
+    def create_dir(self, nl_path):
+        obj_path = nl_path / self.id
+        if not obj_path.is_dir():
+            obj_path.mkdir()
+        else:
+            pass
+
     
 
+    
+def get_newsletters(dir):
+    nl_files = glob.glob(f"{dir}/*.json")
+    nls = []
+    for i in nl_files:
+        with open(i, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            nls.extend(data)
+
+    return nls
+
+
 if __name__ == "__main__":
-    ""
+    # Check for directory structure
+    nl_path = DATA_PATH / "newsletters"
+    if not nl_path.is_dir():
+        nl_path.mkdir(parents=True)
+    # Get newsletters
+    cat_path = DATA_PATH / "categories"
+    newsletters = get_newsletters(cat_path)
+    for nl in tqdm(newsletters):
+        nl_object = Newsletter(nl)
+        # Create directory if missing
+        nl_object.create_dir(nl_path)
+        # Build index file if missing
+        # Build post file if missing
+    
