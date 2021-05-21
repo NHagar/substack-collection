4e94807cb225392bc1db5e025b4694ce7239fea5
NHagar <nicholasrhagar@gmail.com>
2021-05-21 13:47:49 -0500

    start of newsletter collection script

---
+++ newsletter_collect.py
@@ -0,0 +1,25 @@
+import glob
+import json
+import pathlib
+import time
+
+import requests
+from tqdm import tqdm
+
+DATA_PATH = pathlib.Path("./data/")
+
+
+# Check directory structure
+# Make all missing newsletter dirs
+# Build index queue
+# Build post queue
+# Collect indices
+# Collect posts
+
+class Newsletter:
+    def __init__(self):
+        ""
+    
+
+if __name__ == "__main__":
+    ""
