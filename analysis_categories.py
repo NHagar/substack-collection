8728953e25843a0132178256aaadde5bb13a58c9
NHagar <nicholasrhagar@gmail.com>
2021-10-18 21:32:30 -0500

    condense load functions

--- analysis_categories.py
+++ analysis_categories.py
@@ -2,19 +2,5 @@
 import pandas as pd
 
 # %%
-clf = pd.read_csv("./data/newsletters_classified.csv")
-# %%
-clf = clf[clf['id'].notna()]
-
-# %%
-def clean(cat):
-    c = cat.lower()
-    c = c.split("(")[0].strip().replace(" ", "")
-    return c
-
-
-# %%
-clf.loc[:, "cat"] = clf['Category 1'].apply(clean)
-# %%
 clf.groupby("cat").count()
 # %%
---
+++ parsing/load.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def clean(cat):
+    c = cat.lower()
+    c = c.split("(")[0].strip().replace(" ", "")
+    return c
+
+def load_clf(path):
+    clf = pd.read_csv("./data/newsletters_classified.csv")
+    clf = clf[clf['id'].notna()]
+    clf.loc[:, "cat"] = clf['Category 1'].apply(clean)
+
+    return clf
+
+
+def load_posts(path, cols, filters):
+    df = pd.read_parquet(path, 
+                     columns=cols,
+                     filters=filters)
+    
+    return df
