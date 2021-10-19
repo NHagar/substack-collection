bc25b6067ba31d0d6d95cac032729d9f9948f829
NHagar <nicholasrhagar@gmail.com>
2021-10-19 14:14:46 -0500

    moving analysis to R

--- analysis_categories.py
+++ analysis_categories.py
@@ -12,6 +12,7 @@ tqdm.pandas()
 # params
 clf_path = pathlib.Path("./data/newsletters_classified.csv")
 post_path = pathlib.Path("./data/newsletters_pq")
+out_path = pathlib.Path("./data/posts_parsed.csv")
 columns = ["id", 
             "publication_id", 
             "title", 
@@ -55,8 +56,14 @@ posts.loc[:, "domains"] = posts.links_expanded.progress_apply(lambda x: [i.netlo
 df = posts.merge(clf, left_on="publication_id", right_on="id")
 df = df.drop(columns=["id_y"])
 df = df.rename(columns={"id_x": "post_id"})
+# Save processed dataframe
+df = df.drop(columns=["body_html", 
+                      "Unnamed: 0", 
+                      "stripe_user_id", 
+                      "stripe_country", 
+                      "stripe_publishable_key"])
+df.to_csv(out_path)
 
-# TODO: Roll everything up into an analysis pipeline
 
 # %%
 # Calculate overall descriptive counts - 
@@ -67,6 +74,11 @@ desc_overall = links.DescriptiveStats(df)
 # %%
 # Flag internal and repeated links
 marked_links = links.mark_link_classes(df, desc_overall.posts_per_nl)
+
+
+
+
+# %%
 true_links = marked_links[(~marked_links.is_repeat) & (~marked_links.is_self)]
 
 # %%
@@ -82,57 +94,14 @@ true_links = pd.merge(true_links,
          right_index=True)
 
 # %%
-# Newsletter slot variation
-counts = true_links.groupby(["publication_id", "id"]).str_rep.count().reset_index()
+# How many links does a typical newsletter's post have?
+counts = true_links.groupby(["publication_id", "post_id"]).str_rep.count().reset_index()
 counts.groupby("publication_id").str_rep.mean().hist()
 counts.groupby("publication_id").str_rep.std().describe()
 
 # %%
-# Number of available slots, domain prominence
-correlations = true_links.groupby(['publication_id', "id"]) \
-    .agg({"domains_x": "count", "domains_y": "median"}) \
-        .reset_index()[['publication_id', "domains_x", "domains_y"]] \
-            .groupby("publication_id").corr().unstack().iloc[:,1]
-
-correlations.hist()
-
-# %%
-# Entropy
-# Link slot probabilities
-pub_count = df.groupby("publication_id").count().id.rename("posts")
-
-# %%
-t = true_links.groupby(["publication_id", "id"]).apply(lambda x: x.sort_values(by="domains_y", ascending=False)) \
-        .reset_index(drop=True)
-t.loc[:, "slot"] = t.groupby(["publication_id", "id"]).cumcount()
-
-# %%
-slot_counts = t.groupby(["publication_id", "slot"]).count().id
-slot_counts = pd.merge(slot_counts.reset_index(), pub_count.reset_index(), on="publication_id") \
-    .set_index(["publication_id", "slot"])
-slot_counts.loc[:, "pct"] = slot_counts.id / slot_counts.posts
-# %%
-# Domain slot probabilities
-domain_counts = t.groupby(["publication_id", "slot", "domains_x"]).count().str_rep
-domain_counts = pd.merge(slot_counts, domain_counts, left_index=True, right_index=True)
-domain_counts = domain_counts.drop(columns=["posts", "pct"])
-domain_counts.loc[:, "pct"] = domain_counts.str_rep / domain_counts.id
-
-domain_counts = domain_counts.reset_index().drop(columns=["id", "str_rep"])
-slot_counts = slot_counts.reset_index().drop(columns=["id", "posts"])
-# %%
-# Conversion to conditional probabilities
-proba_table = pd.merge(domain_counts, slot_counts, on=["publication_id", "slot"])
-proba_table = proba_table.rename(columns={"pct_x": "proba_domain", "pct_y": "proba_slot"})
-proba_table.loc[:, "probability"] = proba_table.proba_domain * proba_table.proba_slot
-
-# %%
-# Entropy calculation
-ent = proba_table.groupby(["publication_id", "slot"]).agg({"probability": entropy}).reset_index()
-ent[ent.slot<10].groupby("slot").mean().probability
-# %%
-# Dissenting newsletters
-true_links.groupby("publication_id").mean().domains_y.hist()
+g = true_links.groupby(["publication_id", "domains_x"]).count().sort_values(ascending=False, by="post_id")
+g.loc[:, "cumsum"] = g.reset_index().groupby("publication_id")['post_id'].transform(pd.Series.cumsum)
 
 
 # %%
